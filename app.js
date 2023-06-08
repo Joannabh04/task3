3d4aa54f8dc250e3188b5261acf1b49149ffccac
Joannabh04 <131144259+Joannabh04@users.noreply.github.com>
2023-06-08 15:08:15 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log('Hello World!');
+console.log("2");
+
+
+*/
+
+let myLet = "Dette er tekst";
+myLet = "Nytt inhold"
+console.log(myLet);
+const myConst = 5;
+//myConst=(4)
+console.log(myConst);
+
+const darkModeToggle = true;
+//data types
+const myString = "Dette er en string";
+const userName="Killer981"
+//number
+const myNumber=91001822
+
+//booLean true or false
+
+const myBoolen=true
+
+//array (list)
+
+const myArray=[ "bill", "fly", "komet"];
+console.log(myArray(1));
